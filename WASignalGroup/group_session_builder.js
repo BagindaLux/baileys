7c2b1fe55ce36064b638edfa78f39f16eb04f42e
root:~# dd if=/dev/zero <122040630+naruyaizumi@users.noreply.github.com>
2025-05-19 13:36:57 +0700

    Add files via upload

--- WASignalGroup/group_session_builder.js
+++ WASignalGroup/group_session_builder.js
@@ -5,11 +5,10 @@ constructor(senderKeyStore) {
 this.senderKeyStore = senderKeyStore
 }
 async process(senderKeyName, senderKeyDistributionMessage) {
-console.log('\n[GROUP SESSION] 🔐 Memproses session untuk:', senderKeyName)
-console.log('[GROUP SESSION] 📥 Distribusi diterima:', {
-id: senderKeyDistributionMessage.getId(),
-iteration: senderKeyDistributionMessage.getIteration()
-})
+console.log('\n[GROUP SESSION] 🔐 Memproses session baru untuk:', senderKeyName)
+console.log('[GROUP SESSION] 📥 Distribusi kunci diterima:')
+console.log('- ID:', senderKeyDistributionMessage.getId())
+console.log('- Iterasi:', senderKeyDistributionMessage.getIteration())
 const senderKeyRecord = await this.senderKeyStore.loadSenderKey(senderKeyName)
 senderKeyRecord.addSenderKeyState(
 senderKeyDistributionMessage.getId(),
@@ -18,21 +17,21 @@ senderKeyDistributionMessage.getChainKey(),
 senderKeyDistributionMessage.getSignatureKey()
 )
 await this.senderKeyStore.storeSenderKey(senderKeyName, senderKeyRecord)
-console.log('[GROUP SESSION] 🚀 Session diperbarui & disimpan.')
+console.log('[GROUP SESSION] 🚀 Session terenkripsi diperbarui & disimpan.\n')
 }
 async create(senderKeyName) {
 const senderKeyRecord = await this.senderKeyStore.loadSenderKey(senderKeyName)
 console.log('\n[GROUP SESSION] ✨ Membuat session baru untuk:', senderKeyName)
 if (senderKeyRecord.isEmpty()) {
-console.log('[GROUP SESSION] 📦 Tidak ditemukan data sebelumnya. Membuat key baru...')
+console.log('[GROUP SESSION] 📦 Tidak ada session lama. Membuat kunci enkripsi baru...')
 const keyId = keyhelper.generateSenderKeyId()
 const senderKey = keyhelper.generateSenderKey()
 const signingKey = keyhelper.generateSenderSigningKey()
 senderKeyRecord.setSenderKeyState(keyId, 0, senderKey, signingKey)
 await this.senderKeyStore.storeSenderKey(senderKeyName, senderKeyRecord)
-console.log('[GROUP SESSION] 🔑 Key baru berhasil dibuat & disimpan.')
+console.log('[GROUP SESSION] 🔑 Key baru berhasil dibuat dan disimpan.')
 } else {
-console.log('[GROUP SESSION] ♻️ Key lama ditemukan, tidak perlu generate ulang.')
+console.log('[GROUP SESSION] ♻️ Session lama ditemukan. Tidak perlu generate ulang.')
 }
 const state = senderKeyRecord.getSenderKeyState()
 return new SenderKeyDistributionMessage(
